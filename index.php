d1f4801453472cc03456cb6fceee7fcfab017c7a
Brittain Cooke <brittain.p.cooke@wmich.edu>
2017-04-11 21:50:44 -0400

    Rough logo and search bar is perfect

--- index.php
+++ index.php
@@ -34,20 +34,20 @@
     <nav class="navbar navbar-inverse navbar-fixed-top nav-back" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header ">
+            <div class="navbar-header nav-back">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#"><img src="images/Harmonix.png" class="Logo"></a>
+                <a class="navbar-brand" href="#"><img src="images/Harmonix2.png" class="Logo"></a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
             <div class="collapse navbar-collapse " id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav nav-button-back">
+                <ul class="nav navbar-left navbar-nav ">
                     <li>
-                        <a href="#">About</a>
+                        <a href="#">Products</a>
                     </li>
                     <li>
                         <a href="#">Services</a>
@@ -56,8 +56,19 @@
                         <a href="#">Contact</a>
                     </li>
                 </ul>
+                <div class="col-sm-3 col-md-3 navbar-right">
+                <form class="nav navbar-right navbar-nav" role="search">
+              <div class="input-group">
+                  <input type="text" class="form-control" placeholder="Search" name="srch-term" id="srch-term">
+                <div class="input-group-btn">
+                    <button class="btn btn-default" type="submit"><i class="glyphicon glyphicon-search"></i></button>
+                    </div>
+              </div>
+                </form>
+              </div>
             </div>
             <!-- /.navbar-collapse -->
+
         </div>
         <!-- /.container -->
     </nav>
